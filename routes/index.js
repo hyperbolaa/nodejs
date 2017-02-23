6fe5e4b8176a012f0268d62d25540fb594aa7bdf
hyperbolaa <yuchong321@126.com>
2017-02-23 16:51:26 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+    app.get('/',function (req,res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup',require('./signup'));
+    app.use('/signin',require('./signin'));
+    app.use('/signout',require('./signout'));
+    app.use('/posts',require('./posts'));
+    app.use(function (req,res) {
+        if(!res.headersSent){
+            res.render('404');
+        }
+    });
+};
+
+
